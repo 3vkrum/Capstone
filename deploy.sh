f303c0d9eb5d74e9bc58f3740a0c83d126e43342
3vkrum <98079598+3vkrum@users.noreply.github.com>
2024-05-17 15:12:48 +0530

    Modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Logging into EC2 instance"
 ssh -i /Users/trivikram/Developer/mumbai-kp1.pem  ubuntu@3.110.41.80
+echo "Logging into docker"
 docker login -u trivikram97 -p ThinkBig!##
+echo "Pulling image"
 docker pull trivikram97/capstone-prod:latest
